cb3c53518c99831a6ac8732e571028150dbb292e
Laurenn Alecxandra Cruz Vargas <alecxandra.cruz@unitec.edu>
2017-01-20 10:59:04 -0600

    banner de productos

--- mysite/code/ProductHolder.php
+++ mysite/code/ProductHolder.php
@@ -4,7 +4,11 @@ class ProductHolder extends Page{
 	static $has_one = array(
 		'Banner' => 'Image'
 	);
-
+	function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab('Root.Main', new UploadField('Banner', _t('CategoryPage.db_Banner', 'Banner')), 'Content');
+		return $fields;
+	}
 }
 
 class ProductHolder_Controller extends Page_Controller{
